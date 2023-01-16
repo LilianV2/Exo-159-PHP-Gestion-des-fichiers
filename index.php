91ae84b2c7ed54fa99b934fc3b480ad39f49484b
lilia <lilianom59@outlook.fr>
2023-01-16 14:20:55 +0100

    files

--- index.php
+++ index.php
@@ -6,12 +6,24 @@
  */
 // TODO Votre code ici.
 
+$content = file_get_contents('lire.txt');
+if ($content !== false) {
+    echo $content . "<br><br>";
+}
+
 /**
  * 3. Faites la même chose à l'aide d'une boucle et de la fonction fgets(), récupérez les lignes du fichier une à une.
  * 4. Affichez le contenu de chaque ligne dans un nouveau span.
  * --> N'oubliez pas de fermer votre fichier.
  */
 // TODO Votre code ici.
+$content = fopen('lire.txt', 'rb');
+
+while($line = fgets($content)){
+    echo "<span>" . nl2br($line) . "</span>";
+}
+
+fclose($content);
 
 
 /**
@@ -19,7 +31,16 @@
  * --> N'oubliez pas de fermer votre fichier.
  */
 // TODO Votre code ici.
+$content = fopen('lire.txt', 'rb');
 
+while ($char = fgetc($content)) {
+    echo $char;
+    if ($char === " " || $char === "." || $char === "?" || $char === "!"){
+        break;
+    }
+}
+
+fclose($content);
 
 /**
  * 6. Ne cherchez pas la logique dans cet exo ^^
